53385ad70bb725f7830273b7eb7ae1be6d150aca
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-28 14:50:47 +0530

    Add: concurrency example

---
+++ Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var counter = 1
+
+DispatchQueue.main.async {
+    for i in 0...3 {
+        counter = i
+        print("\(counter)")
+    }
+}
+
+for i in 4...9 {
+    counter = i
+    print(counter)
+}
+
+DispatchQueue.main.async {
+    counter = 9
+    print(counter)
+    
+}
